9dfb713e6e7582b666b81970e95fb8eb4746083c
millernoam <71715972+millernoam@users.noreply.github.com>
2020-10-06 10:03:59 -0400

    Add files via upload

--- operator.r
+++ operator.r
@@ -1,6 +1,9 @@
-setwd("C:/Research/projects/Jovan model")
+#this script runs the model
+
+setwd("C:/Research/projects/Jovan model")  #folder where the data are
 
 source("Network.R")
 source("functions.R")
 
+#run the model 10 times
 lapply(1:10, function(n)source("runfilenew.R"))
