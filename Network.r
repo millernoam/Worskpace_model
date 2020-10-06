5eed59ee3bb42a0e73736e10ff466b9614defa49
millernoam <71715972+millernoam@users.noreply.github.com>
2020-10-06 10:02:18 -0400

    Add files via upload

--- Network.r
+++ Network.r
@@ -1,25 +1,21 @@
+#This script loads in and sets up the physical space networks
+
 library(readxl)
-library(igraph) 
-setwd("C:/Research/projects/Jovan model")
-Floor1 <- read_excel("Floor 1 Network.xlsx")
-Floor2 <- read_excel("Floor 2 Network.xlsx")
-Floor3 <- read_excel("Floor 3 Network.xlsx")
+library(igraph)
+
+setwd("C:/Research/projects/Jovan model")             #folder where the data are
+floorPlan <- read_excel("Floor 1 Network.xlsx")     #read in the network
+
 #clean up data
-Floor1 <- subset(Floor1, select = -c(1))
-Floor2 <- subset(Floor2, select = -c(1))
-Floor3 <- subset(Floor3, select = -c(1))
-Floor1 <- `rownames<-`(Floor1, colnames(Floor1))
-Floor2 <- `rownames<-`(Floor2, colnames(Floor2))
-Floor3 <- `rownames<-`(Floor3, colnames(Floor3))
+floorPlan <- subset(floorPlan, select = -c(1))
+floorPlan <- `rownames<-`(floorPlan, colnames(floorPlan))
+
 #convert data into matrix
-F1 <- data.matrix(Floor1)
-F2 <- data.matrix(Floor2)
-F3 <- data.matrix(Floor3)
+F1 <- data.matrix(floorPlan)
+
 #create graph
 F1Graph <- graph_from_adjacency_matrix(F1)
-F2Graph <- graph_from_adjacency_matrix(F2)
-F3Graph <- graph_from_adjacency_matrix(F3)
 
-#read in attractivenesses
+#read in node attractiveness list
 F1att <- read_xlsx("network node codebook.xlsx", 1)
-F1nodeatt = as.list(att[3])[[1]]  #list of node attractivenesses
+F1nodeatt = as.list(att[3])[[1]]
