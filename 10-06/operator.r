6ba7f12d70b420e4867ff6cd105765063d9310c2
millernoam <71715972+millernoam@users.noreply.github.com>
2020-10-08 14:52:39 -0400

    Delete operator.r

--- 10-06/operator.r
+++
@@ -1,8 +0,0 @@
-setwd("C:/Users/Jovan/Documents/Model split up")
-
-source("Network.R")
-source("Parameters.R")
-setwd("C:/Users/Jovan/Documents/Model split up")
-source("functions.R")
-
-lapply(1:10, function(n)source("runfilenew.R"))
