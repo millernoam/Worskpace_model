1cf5fc1ef3852df728444face63d06f458297daf
millernoam <71715972+millernoam@users.noreply.github.com>
2020-10-08 17:34:18 -0400

    Add files via upload

--- Network.r
+++ Network.r
@@ -3,21 +3,37 @@
 library(readxl)
 library(igraph)
 
-setwd("C:/Research/projects/Jovan model")             #folder where the data are
-floorPlan <- read_excel("network node codebook.xlsx",2)
-
-#clean up data
+#read in floor plan and clean up data
+floorPlan <- read_excel(mynet, 2)
 floorPlan <- subset(floorPlan, select = -c(1))
 floorPlan <- `rownames<-`(floorPlan, colnames(floorPlan))
-
-#convert data into matrix
-F1 <- data.matrix(floorPlan)
+F1 <- data.matrix(floorPlan)  #convert data into matrix
 
 #create graph
 F1Graph <- graph_from_adjacency_matrix(F1)
 
 #read in node attractiveness list
-node_attractiveness<-c(read_xlsx("network node codebook.xlsx", 1)[[3]])  #list of node attractiveness
+node_attractiveness<-c(read_xlsx(mynet, 1)[[3]])  #list of node attractiveness
+
+#read in other parameters
+ws_spots = na.omit(c(read_excel(mynet,3)[[1]]))        #list of workstations
+kitchen_spot = na.omit(c(read_excel(mynet,3)[[2]]))    #kitchen
+sr_spots = na.omit(c(read_excel(mynet,3)[[3]]))        #socializing/relaxing spots
+wc_spot = na.omit(c(read_excel(mynet,3)[[4]]))         #washroom
+out_spots = na.omit(c(read_excel(mynet,3)[[5]]))       #outside spots
+
+total_time = (c(read_excel(mynet,5)[[1]]))              #length of the day
+numagents = (c(read_excel(mynet,5)[[2]]))               #number of agents
+num_days = (c(read_excel(mynet,5)[[3]]))                #number of days to run
+
+foodmean = (c(read_excel(mynet,4)[[2]][3]))             #Mean of foodiness
+foodsd = (c(read_excel(mynet,4)[[3]][3]))               #SD of foodiness
+socmean = (c(read_excel(mynet,4)[[2]][1]))              #Mean of sociability
+socsd = (c(read_excel(mynet,4)[[3]][1]))                #SD of sociability
+workmean = (c(read_excel(mynet,4)[[2]][2]))             #Mean of work ethic
+worksd = (c(read_excel(mynet,4)[[3]][2]))               #SD of work ethic
+bladmean = (c(read_excel(mynet,4)[[2]][4]))             #Mean of bladdercap
+bladsd = (c(read_excel(mynet,4)[[3]][4]))               #SD of bladdercap
 
 ########################      Make random network       ###########################################################################################################################3
 
--- operator.r
+++ operator.r
@@ -1,8 +1,8 @@
-setwd("C:/Users/Jovan/Documents/Model split up")
+setwd("C:/Users/Jovan/Documents/Model split up")  #enter the path to all files
+
+mynet = "network node codebook.xlsx"              #enter file name for network
 
 source("Network.R")
-source("Parameters.R")
-setwd("C:/Users/Jovan/Documents/Model split up")
 source("functions.R")
 
 lapply(1:10, function(n)source("runfilenew.R"))
