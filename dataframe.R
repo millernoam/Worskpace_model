388ae30a42b6ae9ab1b4bbeeb07659e9cc4c3003
Jovan652 <53873539+Jovan652@users.noreply.github.com>
2021-01-31 13:48:14 -0500

    Add files via upload
    fixed up

--- dataframe.R
+++ dataframe.R
@@ -1,28 +1,28 @@
 directory = "C:/Users/Jovan/Documents/Jovan sims/Jovan sims/A3"
-dataframe <- function(directory){
-  setwd(directory)                                               #setwd
+dataframe <- function(dir){
+  setwd(dir)                                               #setwd
   folders <<- list.files()                                       #list all folders
   hapslist <<- rep(list(), length = length(folders))
   perslist <<- rep(list(), length = length(folders))
-  list5 <- rep(list(), length = 5)
+  list5 <<- rep(list(), length = 5)
   
   for(i in 1:length(folders)){                                   #iterate through all folders
-    setwd(directory)                                             #setwd to directory stated so it can go into another folder
+    setwd(dir)                                             #setwd to directory stated so it can go into another folder
     setwd(folders[[i]])                                          #set wd as the folders iterated through
-    hapsfiles <- list.files(pattern = "haps")                    #list all files in that folder
-    persfiles <- list.files(pattern = "pers")
+    hapsfiles <<- list.files(pattern = "haps")                    #list all files in that folder
+    persfiles <<- list.files(pattern = "pers")
     listhapsfiles <- rep(list(), length = length(hapsfiles))
     listpersfiles <- rep(list(), length = length(persfiles))
-    hapslist[[i]] <- append(hapslist[[i]], listhapsfiles)
-    perslist[[i]] <- append(perslist[[i]], listpersfiles)
+    hapslist[[i]] <<- append(hapslist[[i]], listhapsfiles)
+    perslist[[i]] <<- append(perslist[[i]], listpersfiles)
     
     for(p in 1:length(persfiles)){                            
       file <- read.csv(persfiles[[p]])                               #choose file from pers files
       file = file[-1]                                                #take first column out
-      perslist[[i]][[p]] <- append(perslist[[i]][[p]], list5)
+      perslist[[i]][[p]] <<- append(perslist[[i]][[p]], list5)
       
       for(c in 1:5){
-         perslist[[i]][[p]][[c]] <- append(perslist[[i]][[p]][[c]], file[,c])
+         perslist[[i]][[p]][[c]] <<- append(perslist[[i]][[p]][[c]], file[,c])
 
       }
     }
@@ -32,7 +32,7 @@ dataframe <- function(directory){
         finalhaps <- file[nrow(file),]
         
         for(a in 1:length(finalhaps)){
-          hapslist[[i]][[h]] <- append(hapslist[[i]][[h]], finalhaps[[a]])
+          hapslist[[i]][[h]] <<- append(hapslist[[i]][[h]], finalhaps[[a]])
       } 
     }
   }
